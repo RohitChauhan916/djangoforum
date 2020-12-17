8790deae4798f3b4bdd78ea55046295fc8d24550
RohitChauhan916 <rohitchauhan916@gmail.com>
2020-12-17 10:31:14 +0530

    add postgress sql

--- happiness/settings.py
+++ happiness/settings.py
@@ -106,13 +106,24 @@ WSGI_APPLICATION = 'happiness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'aipl',
+            'USER': 'parivaar_admin',
+            'PASSWORD': 'Parivaar@123',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
