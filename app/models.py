44612cdba39fa2f5b867e1ee946261e31084461f
RohitChauhan916 <rohitchauhan916@gmail.com>
2020-12-17 12:49:56 +0530

    add primary key in all model

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
 
 class UserProfile(models.Model):
-    id = models.AutoField(primary_key = True, null=False)
+    id = models.AutoField(primary_key = True, null=True)
     user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE, null=True)
     emp_code = models.CharField(verbose_name="EMP Code", max_length=100, blank=True, null=True, unique=True)
     profile_photo = models.ImageField(verbose_name="Profile Photo", upload_to='Profile', blank=True, null=True)
@@ -30,7 +30,6 @@ class UserProfile(models.Model):
 
 
 class discussion(models.Model):
-    discuss_id = models.AutoField(primary_key=True, null=False)
     username = models.CharField(null=True, max_length=100)
     description = models.TextField(max_length=200, verbose_name="Description", blank=True, null=True)
     photo = models.ImageField(verbose_name="Image", upload_to='content', blank=True, null=True)
@@ -41,12 +40,10 @@ class discussion(models.Model):
         return self.description
 
 class Likes(models.Model):
-    like_id = models.AutoField(primary_key=True, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     discuss = models.ForeignKey(discussion, on_delete=models.CASCADE)
 
 class suggestion(models.Model):
-    suggest_id = models.AutoField(primary_key=True, null=False)
     emp_code = models.CharField(verbose_name="Emp Code", max_length=200, null=True, blank=True)
     subject = models.CharField(verbose_name="Subject", max_length=200, blank=True, null=True)
     message = models.TextField(verbose_name="Message", max_length=500, null=True, blank=True)
@@ -57,7 +54,6 @@ class suggestion(models.Model):
 
 
 class advertisements(models.Model):
-    a_id = models.AutoField(primary_key=True, null=False)
     image = models.ImageField(verbose_name="Image", upload_to='advertisement', null=True, blank=True)
     link = models.CharField(verbose_name="Link", max_length=200, null=True, blank=True)
     m_img = models.ImageField(upload_to="Banner", verbose_name="Mobile Banner", null=True, blank=True)
@@ -65,7 +61,6 @@ class advertisements(models.Model):
         return str(self.a_id)
 
 class announce(models.Model):
-    an_id = models.AutoField(primary_key=True, null=False)
     title = models.CharField(verbose_name="Announcements", max_length=250, null=True, blank=True)
     desc = RichTextUploadingField()
     
@@ -73,7 +68,6 @@ class announce(models.Model):
         return self.title
     
 class event(models.Model):
-    event_id = models.AutoField(primary_key=True, null=False)
     Title = models.CharField(max_length=255, verbose_name="Event Title", null=True, blank=True)
     image = models.ImageField(verbose_name="Event Photo", upload_to="Event", null=True, blank=True)
     event_slug = models.CharField(max_length=255)
@@ -83,7 +77,6 @@ class event(models.Model):
         return self.Title
 
 class gallery(models.Model):
-    gallery_id = models.AutoField(primary_key=True, null=False)
     event_for = models.ForeignKey(event, on_delete=models.SET_DEFAULT, default=1)
     title = models.CharField(max_length=255, verbose_name="Gallery Title", null=True, blank=True)
     desc = models.TextField()
@@ -92,7 +85,6 @@ class gallery(models.Model):
         return self.title
 
 class galleryPhoto(models.Model):
-    gp_id = models.AutoField(primary_key=True, null=False)
     gallery = models.ForeignKey(gallery, on_delete=models.CASCADE)
     photo = models.FileField(verbose_name="Gallery Photo", upload_to="Gallery", null=True, blank=True)
 
@@ -100,14 +92,12 @@ class galleryPhoto(models.Model):
         return str(self.gp_id)
 
 class performer(models.Model):
-    p_id = models.AutoField(primary_key=True, null=False)
     title = models.CharField(max_length=255, verbose_name="Title", null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 class company(models.Model):
-    c_id = models.AutoField(primary_key=True, null=False)
     perform = models.ForeignKey(performer, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, verbose_name="Performer Name", null=True, blank=True)
     image = models.ImageField(upload_to="Performer", null=True, blank=True)
@@ -117,7 +107,6 @@ class company(models.Model):
         return self.company_name
 
 class banner(models.Model):
-    img_id = models.AutoField(primary_key=True, null=False)
     img = models.ImageField(upload_to="Banner", verbose_name="Banner", null=True, blank=True)
     def __str__(self):
         return str(self.img_id)
