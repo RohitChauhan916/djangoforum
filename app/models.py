445e733e7e7b9c57f858d9c8c4dad40d65bddb0c
RohitChauhan916 <rohitchauhan916@gmail.com>
2020-12-17 11:42:28 +0530

    add primary key in all model

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
 
 class UserProfile(models.Model):
-    UserProfile_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE, null=True)
     emp_code = models.CharField(verbose_name="EMP Code", max_length=100, blank=True, null=True, unique=True)
     profile_photo = models.ImageField(verbose_name="Profile Photo", upload_to='Profile', blank=True, null=True)
