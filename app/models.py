c975cb705f4c1f08c53bf3345710438f0fd7b825
RohitChauhan916 <rohitchauhan916@gmail.com>
2020-12-17 15:22:18 +0530

    add primary key in all model

--- app/models.py
+++ app/models.py
@@ -7,32 +7,31 @@ from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
 
 class UserProfile(models.Model):
-    id = models.BigIntegerField(primary_key = True)
-    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
-    emp_code = models.CharField(verbose_name="EMP Code", max_length=100, null=True, unique=True)
-    profile_photo = models.ImageField(verbose_name="Profile Photo", upload_to='Profile',   null=True)
-    dob = models.CharField(verbose_name="D.O.B", max_length=50,   null=True)
-    department = models.CharField(verbose_name="Department", max_length=100, null=True)
-    phone_number = models.CharField(verbose_name="Phone Number", max_length=20, null=True)
-    web_address = models.CharField(verbose_name="Web Address", max_length=100, null=True)
-    location = models.CharField(verbose_name="Location", max_length=100, null=True)
-    hire_date = models.CharField(verbose_name="Hire Date", max_length=100, null=True)
-    about = models.TextField(verbose_name="About", max_length=100, null=True)
-    gender = models.CharField(verbose_name="Gender", max_length=20, null=True)
-    zone = models.CharField(verbose_name="Zone", max_length=100,   null=True)
-    aniversary = models.CharField(verbose_name="Aniversary", max_length=100,   null=True)
-    designation = models.CharField(verbose_name="Designation", max_length=100,   null=True)
-    reporting_to = models.CharField(verbose_name="Reporting To", max_length=100,   null=True)
-    cover_photo = models.ImageField(verbose_name="Cover", upload_to="cover",   null=True)
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE, null=True)
+    emp_code = models.CharField(verbose_name="EMP Code", max_length=100, blank=True, null=True, unique=True)
+    profile_photo = models.ImageField(verbose_name="Profile Photo", upload_to='Profile', blank=True, null=True)
+    dob = models.CharField(verbose_name="D.O.B", max_length=50, blank=True, null=True)
+    department = models.CharField(verbose_name="Department", max_length=100, blank=True, null=True)
+    phone_number = models.CharField(verbose_name="Phone Number", max_length=20, blank=True, null=True)
+    web_address = models.CharField(verbose_name="Web Address", max_length=100, blank=True, null=True)
+    location = models.CharField(verbose_name="Location", max_length=100, blank=True, null=True)
+    hire_date = models.CharField(verbose_name="Hire Date", max_length=100, blank=True, null=True)
+    about = models.TextField(verbose_name="About", max_length=100, blank=True, null=True)
+    gender = models.CharField(verbose_name="Gender", max_length=20, blank=True, null=True)
+    zone = models.CharField(verbose_name="Zone", max_length=100, blank=True, null=True)
+    aniversary = models.CharField(verbose_name="Aniversary", max_length=100, blank=True, null=True)
+    designation = models.CharField(verbose_name="Designation", max_length=100, blank=True, null=True)
+    reporting_to = models.CharField(verbose_name="Reporting To", max_length=100, blank=True, null=True)
+    cover_photo = models.ImageField(verbose_name="Cover", upload_to="cover", blank=True, null=True)
     
     def __str__(self):
         return str(self.emp_code)
 
 
 class discussion(models.Model):
-    discuss_id = models.AutoField(primary_key=True, null=False)
     username = models.CharField(null=True, max_length=100)
-    description = models.TextField(max_length=200, verbose_name="Description", null=True)
+    description = models.TextField(max_length=200, verbose_name="Description", blank=True, null=True)
+    photo = models.ImageField(verbose_name="Image", upload_to='content', blank=True, null=True)
     date_published = models.DateTimeField(auto_now_add=True)
     profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True)
     
@@ -40,14 +39,12 @@ class discussion(models.Model):
         return self.description
 
 class Likes(models.Model):
-    like_id = models.AutoField(primary_key=True, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     discuss = models.ForeignKey(discussion, on_delete=models.CASCADE)
 
 class suggestion(models.Model):
-    suggest_id = models.AutoField(primary_key=True, null=False)
     emp_code = models.CharField(verbose_name="Emp Code", max_length=200, null=True, blank=True)
-    subject = models.CharField(verbose_name="Subject", max_length=200,   null=True)
+    subject = models.CharField(verbose_name="Subject", max_length=200, blank=True, null=True)
     message = models.TextField(verbose_name="Message", max_length=500, null=True, blank=True)
     date_published = models.DateTimeField(default=datetime.now())
 
@@ -56,7 +53,7 @@ class suggestion(models.Model):
 
 
 class advertisements(models.Model):
-    a_id = models.AutoField(primary_key=True, null=False)
+    a_id = models.AutoField(primary_key=True)
     image = models.ImageField(verbose_name="Image", upload_to='advertisement', null=True, blank=True)
     link = models.CharField(verbose_name="Link", max_length=200, null=True, blank=True)
     m_img = models.ImageField(upload_to="Banner", verbose_name="Mobile Banner", null=True, blank=True)
@@ -64,7 +61,7 @@ class advertisements(models.Model):
         return str(self.a_id)
 
 class announce(models.Model):
-    an_id = models.AutoField(primary_key=True, null=False)
+    an_id = models.AutoField(primary_key=True)
     title = models.CharField(verbose_name="Announcements", max_length=250, null=True, blank=True)
     desc = RichTextUploadingField()
     
@@ -72,7 +69,6 @@ class announce(models.Model):
         return self.title
     
 class event(models.Model):
-    event_id = models.AutoField(primary_key=True, null=False)
     Title = models.CharField(max_length=255, verbose_name="Event Title", null=True, blank=True)
     image = models.ImageField(verbose_name="Event Photo", upload_to="Event", null=True, blank=True)
     event_slug = models.CharField(max_length=255)
@@ -82,7 +78,6 @@ class event(models.Model):
         return self.Title
 
 class gallery(models.Model):
-    gallery_id = models.AutoField(primary_key=True, null=False)
     event_for = models.ForeignKey(event, on_delete=models.SET_DEFAULT, default=1)
     title = models.CharField(max_length=255, verbose_name="Gallery Title", null=True, blank=True)
     desc = models.TextField()
@@ -91,7 +86,7 @@ class gallery(models.Model):
         return self.title
 
 class galleryPhoto(models.Model):
-    gp_id = models.AutoField(primary_key=True, null=False)
+    gp_id = models.AutoField(primary_key=True)
     gallery = models.ForeignKey(gallery, on_delete=models.CASCADE)
     photo = models.FileField(verbose_name="Gallery Photo", upload_to="Gallery", null=True, blank=True)
 
@@ -99,14 +94,14 @@ class galleryPhoto(models.Model):
         return str(self.gp_id)
 
 class performer(models.Model):
-    p_id = models.AutoField(primary_key=True, null=False)
+    p_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255, verbose_name="Title", null=True, blank=True)
 
     def __str__(self):
         return self.title
 
 class company(models.Model):
-    c_id = models.AutoField(primary_key=True, null=False)
+    c_id = models.AutoField(primary_key=True)
     perform = models.ForeignKey(performer, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, verbose_name="Performer Name", null=True, blank=True)
     image = models.ImageField(upload_to="Performer", null=True, blank=True)
@@ -116,7 +111,7 @@ class company(models.Model):
         return self.company_name
 
 class banner(models.Model):
-    img_id = models.AutoField(primary_key=True, null=False)
+    img_id = models.AutoField(primary_key=True)
     img = models.ImageField(upload_to="Banner", verbose_name="Banner", null=True, blank=True)
     def __str__(self):
         return str(self.img_id)
--- happiness/settings.py
+++ happiness/settings.py
@@ -106,24 +106,12 @@ WSGI_APPLICATION = 'happiness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-#if DEBUG:
-#    DATABASES = {
-#       'default': {
-#           'ENGINE': 'django.db.backends.sqlite3',
-#           'NAME': BASE_DIR / 'db.sqlite3',
-#        }
-#    }
-#else:
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'aipl',
-        'USER': 'parivaar_admin',
-        'PASSWORD': 'Parivaar@123',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-} 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
