1c9af27c6acf1315368423bd10cf103b0bbc9bac
RohitChauhan916 <rohitchauhan916@gmail.com>
2021-02-03 16:29:01 +0530

    pushed new

--- happiness/settings.py
+++ happiness/settings.py
@@ -33,7 +33,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['31.220.59.205']
 
 # config ckeditor
 CKEDITOR_CONFIGS = {
@@ -106,12 +106,24 @@ WSGI_APPLICATION = 'happiness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'parivaar',
+        'USER': 'admin',
+        'PASSWORD': 'Rohit@123',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
+
 
 
 # Password validation
