af8fb28e3abdb718ceaf12617d909280c76cd21b
RohitChauhan916 <rohitchauhan916@gmail.com>
2021-02-09 14:03:45 +0530

    add content

--- app/views.py
+++ app/views.py
@@ -28,7 +28,7 @@ def register(request):
                 return redirect("/register")
             else:
                 for msg in form.error_messages:
-                    print(unicode(form.error_messages[msg]))
+                    print(form.error_messages[msg])
     if request.user.is_authenticated:
         return redirect('/')
     else:
