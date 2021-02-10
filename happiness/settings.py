14c475a3056a9e1e749a8f539dca08faf8e821d3
RohitChauhan916 <rohitchauhan916@gmail.com>
2021-02-10 18:34:31 +0530

    add email settings

--- happiness/settings.py
+++ happiness/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = '%q1lh5h#y*80i6_024l26dk^&z%5-d!j2ee1tan#5f16nx%3zh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'rohitc.aiplbrandbuzz@gmail.com'
-EMAIL_HOST_PASSWORD = 'sec@!((%urity'
+EMAIL_HOST = 'smtp.hostinger.in'
+EMAIL_HOST_USER = 'aipl-parivaar@aiplabro.com'
+EMAIL_HOST_PASSWORD = 'Parivaar@123'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
