90f80214177fd679531bcf31b1836858595e1fa1
RohitChauhan916 <rohitchauhan916@gmail.com>
2021-02-13 18:04:36 +0530

    add media_root folder

--- happiness/settings.py
+++ happiness/settings.py
@@ -171,7 +171,7 @@ MEDIA_URL = '/media/'
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 CKEDITOR_IMAGE_BACKEND = "pillow"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'app/static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'app/static/media')
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
