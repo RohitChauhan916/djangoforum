8843280608389e7af95a8a49e7a1d8a8b82887f9
RohitChauhan916 <rohitchauhan916@gmail.com>
2021-04-16 12:11:39 +0530

    database host change

--- happiness/settings.py
+++ happiness/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
         'NAME': 'parivaar',
         'USER': 'admin',
         'PASSWORD': 'Parivaar@123',
-        'HOST': '31.220.59.205',
+        'HOST': '*',
         'PORT': '5432'
     }
 }
